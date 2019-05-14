1718cb19982678a742105707b4b40771595ca99b
Rajarshi Yelamati <rajarshi@Rajarshis-MacBook-Pro.local>
2019-05-13 20:55:28 -0400

    added card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { NavBarComponent } from './global/nav-bar/nav-bar.component';
+import { CardComponent } from './shared/card/card.component';
+import { CourseCardComponent } from './shared/course-card/course-card.component';
+import { ReviewCardComponent } from './shared/review-card/review-card.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    CardComponent,
+    CourseCardComponent,
+    ReviewCardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
